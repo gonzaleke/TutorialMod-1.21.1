bdd5925c7146b42ca5cf6f8b8a2ddbed9b4f1912
gonza <gonza8976543@gmail.com>
2024-12-24 18:18:05 -0300

    Initial commit

---
+++ src/main/java/net/gonza/tutorialmod/TutorialModClient.java
@@ -0,0 +1,12 @@
+package net.gonza.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialModClient implements ClientModInitializer {
+
+
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
